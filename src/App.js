33233342c0612c9c87a09c33b529d0dca56861c1
Eric Tillberg <eric.tillberg@gmail.com>
2016-11-19 12:32:55 -0500

    Add authentication popup that leads nowhere

--- src/App.js
+++ src/App.js
@@ -1,12 +1,44 @@
 import React, { Component } from 'react';
+import firebase from 'firebase';
 import logo from './logo.svg';
 import './App.css';
 
-const App = (props) => (
-  <div className="App">
-    Header
-    {props.children}
-  </div>
-)
+const App = React.createClass({
 
-export default App; 
+  componentDidMount() {
+    // Initialize Firebase
+    var config = {
+      apiKey: "AIzaSyD5LG0tGT5ViExMwYkyDiRjvFqv1dIMnWI",
+      authDomain: "team-success.firebaseapp.com",
+      databaseURL: "https://team-success.firebaseio.com",
+      storageBucket: "",
+      messagingSenderId: "702172172434"
+    };
+    firebase.initializeApp(config);
+  }
+
+  signIn() {
+    var provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+      var token = result.credential.accessToken;
+      var user = result.user;
+
+    }).catch(function(error) {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      var email = error.email;
+      var credential = error.credential;
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <a href="#" onClick={() => {this.signIn()}}>Sign in here!</a>
+        {this.props.children}
+      </div>
+    );
+  }
+});
+
+export default App;
