38f4e997d96051fca137fa3528409e010009327d
BeejLuig <bjcantlupe@gmail.com>
2016-11-20 13:36:17 -0500

    fix current lesson sizing

--- src/teacherDashboard/CurrentLessons.jsx
+++ src/teacherDashboard/CurrentLessons.jsx
@@ -19,7 +19,7 @@ const CurrentLessons = React.createClass({
   completedTask(lesson){
     console.log("completedTask")
     this.setState({module: true, question: "Did you get paid?", lesson: lesson})
-    
+
   },
   closeModal(){
     this.setState({module: false})
@@ -34,14 +34,17 @@ const CurrentLessons = React.createClass({
       .map((lesson, idx) => {
         let lessonObj = this.props.lessons
          return (
-          <li key={idx} className="currentItem">
-              <p>{this.props.lessons[lesson].studentName
-                  + ' - ' + this.props.lessons[lesson].lessonDate
-                  + ' at ' + this.props.lessons[lesson].lessonTime
-                  }</p>
-            <button onClick={()=>this.completedTask(lesson)}>Complete</button>
-            <button onClick={this.unCompletedTask}>X</button>
-          </li>
+           <li key={idx} className="currentItem">
+             <div className="lesson-info">
+               <p className="student-name">{this.props.lessons[lesson].studentName}</p>
+                 <div className="lesson-buttons">
+                   <button onClick={()=>this.completedTask(lessonObj[lesson].startDate)}>Complete</button>
+                   <button onClick={this.unCompletedTask}>X</button>
+                 </div>
+               <p>Date: {this.props.lessons[lesson].lessonDate}</p>
+               <p>Time: {this.props.lessons[lesson].lessonTime}</p>
+             </div>
+           </li>
         )
       })
     return (
