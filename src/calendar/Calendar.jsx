b111f8f88b0439724bbb44e09cb268ec21d99500
Jack Huang <jack7100@gmail.com>
2016-11-19 23:34:12 -0500

    Move dummy array into initial state and add function to select slots

--- src/calendar/Calendar.jsx
+++ src/calendar/Calendar.jsx
@@ -7,22 +7,40 @@ import CalendarService from '../services/CalendarService';
 BigCalendar.momentLocalizer(moment);
 
 const Calendar = React.createClass({
+  getInitialState() {
+    return this.state = {
+     myEvents: [{
+                  'title': 'All Day Event',
+                  'allDay': true,
+                  'start': new Date(2016, 11, 19),
+                  'end': new Date(2016, 11, 19)
+                },
+              ],
+    }
+  },
+
+  setSlots(info) {
+    this.setState({myEvents: 
+      this.state.myEvents.concat([{
+        'title': 'Available',
+        'start': info.start,
+        'end': info.end
+      }])
+    })
+  },
+
   render() {
-    const myEvents = [
-      {
-        'title': 'All Day Event',
-        'allDay': true,
-        'start': new Date(2016, 11, 19),
-        'end': new Date(2016, 11, 19)
-      },
-    ]
     return (
       <BigCalendar
-        events={myEvents}
+        events={this.state.myEvents}
         startAccessor='startDate'
         endAccessor='endDate'
         timeslots={1}
         defaultView='week'
+        selectable={true}
+        onSelectSlot={(slotInfo) => {
+          this.setSlots(slotInfo)
+        }}
       />
     )
   }
